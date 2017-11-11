24a91bc9b34aacc722f87ff4996937de51920e05
legomolina <legomolina@gmail.com>
2017-11-11 18:40:51 +0100

    Added /api/v1/ to all routes

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -3,7 +3,7 @@
 use app\controllers\RoutesController;
 use app\controllers\StationsController;
 
-$app->get("/routes", RoutesController::class . ":getRoute");
+$app->get("/api/v1/routes", RoutesController::class . ":getRoute");
 
-$app->get("/stations", StationsController::class . ":getAllStations");
-$app->get("/stations/converter/{station}", StationsController::class . ":convertStation");
+$app->get("/api/v1/stations", StationsController::class . ":getAllStations");
+$app->get("/api/v1/stations/converter/{station}", StationsController::class . ":convertStation");
